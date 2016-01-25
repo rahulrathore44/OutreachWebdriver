407f2454763e94316f3d41bffdf185db6c63cf4f
Rahul Singh Rathore <rahulrathore_2007@yahoo.co.in>
2016-01-25 20:16:48 +0800

    :1234: EPPlus Package Checked in

---
+++ Common.Helpers/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.Helpers
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void TestExcel()
+        {
+            var stream = new FileStream(@"C:\Keyword\Book1.xlsx", FileMode.Open, FileAccess.Read);
+            var reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            DataTable table = reader.AsDataSet().Tables["Sheet1"];
+            var t = table.CreateDataReader();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("DataType : " + t.GetDataTypeName(i));
+            }
+
+            stream.Close();
+            reader.Close();
+        }
+    }
+}
---
+++ Common.Helpers/ExcelHelpers/ExcelReaderHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel;
+using OfficeOpenXml;
+
+namespace Common.Helpers.ExcelHelpers
+{
+    public class ExcelReaderHelper
+    {
+        #region Fields
+
+        private readonly static IDictionary<string, ExcelWorksheet> _cache;
+        private static FileInfo _fileInfo;
+        private static ExcelPackage _package;
+        private static ExcelWorksheet _excelWorksheet;
+
+        #endregion
+
+        #region Constructor
+
+        static ExcelReaderHelper()
+        {
+            _cache = new Dictionary<string, ExcelWorksheet>();
+        }
+
+
+        #endregion
+
+        #region Private
+        private static ExcelWorksheet GetExcelReader(string xlPath, string sheetName)
+        {
+            if (_cache.ContainsKey(sheetName))
+            {
+                _excelWorksheet = _cache[sheetName];
+            }
+            else
+            {
+                _fileInfo = new FileInfo(xlPath);
+                _package = new ExcelPackage(_fileInfo);
+                _excelWorksheet = _package.Workbook.Worksheets[sheetName];
+                _cache.Add(sheetName, _excelWorksheet);
+            }
+            return _excelWorksheet;
+        }
+
+
+        #endregion
+
+        #region Public
+
+        public static int GetTotalRows(string xlPath, string sheetName)
+        {
+            var reader = GetExcelReader(xlPath, sheetName);
+            return reader.Cells.Count();
+        }
+
+        //public static DataTable GetDataTable(string xlPath, string sheetName)
+        //{
+        //    var reader = GetExcelReader(xlPath, sheetName);
+        //    return reader.AsDataSet().Tables[sheetName];
+        //}
+
+        //public static T GetCellValue<T>(string xlPath, string sheetName,int row ,int column) 
+        //{
+        //    var dataTable = GetDataTable(xlPath, sheetName);
+        //    var datareader = dataTable.CreateDataReader();
+        //    return "";
+
+        //}
+
+        #endregion
+
+
+    }
+}
