78a6e1ec08f61af9cd485d1140566508a9ebd08f
Rahul Singh Rathore <rahulrathore_2007@yahoo.co.in>
2016-02-03 12:36:00 +0800

    :smile: Added the extension method for entering the text if empty

---
+++ Common.Helpers/ExtensionClass/WebElementExtensionClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Common.Helpers.ExtensionClass
+{
+    public static class WebElementExtensionClass
+    {
+        public static void SendKeyIfEmpty(this IWebElement element,string text)
+        {
+            if(string.Empty.Equals(element.Text))
+                element.SendKeys(text);
+        }
+    }
+}
--- Cross.Examine/TestCases/TestBusinessAddress.cs
+++ Cross.Examine/TestCases/TestBusinessAddress.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Common.Helpers.BaseClasses;
 using Common.Helpers.ComponentHelper;
+using Common.Helpers.ExtensionClass;
 using Common.Helpers.Settings;
 using Cross.Examine.PageObject.DetailPage;
 using Cross.Examine.PageObject.DetailPage.DemographicPage;
@@ -21,6 +22,7 @@ namespace Cross.Examine.TestCases
         {
             NavigationalHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite() + Properties.Settings.Default.GSBSAlumnis);
             var dPage = new DemographicPageClass(ObjectRepository.Driver);
+            dPage.Email.SendKeyIfEmpty("abc@gmail.com");
             dPage.NextBtn.Click();
             dPage.SelectProfessionalSuffix("ACNP", "ACNP-BC", "BA", "CHCA");
         }
